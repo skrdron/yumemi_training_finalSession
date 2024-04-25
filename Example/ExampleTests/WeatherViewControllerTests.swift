bf0de4e0ddd4a05b32b50798645d19256c77ac74
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-04-25 12:23:25 +0900

    "Cannot find 'R' in scope"出現により一時コミットで再度クローン予定

--- Example/ExampleTests/WeatherViewControllerTests.swift
+++ Example/ExampleTests/WeatherViewControllerTests.swift
@@ -14,11 +14,14 @@ class WeatherViewControllerTests: XCTestCase {
 
     var weahterViewController: WeatherViewController!
     var weahterModel: WeatherModelMock!
+    var disasterModel : DisasterModelMock!
     
     override func setUpWithError() throws {
         weahterModel = WeatherModelMock()
+        disasterModel = DisasterModelMock()
         weahterViewController = R.storyboard.weather.instantiateInitialViewController()!
         weahterViewController.weatherModel = weahterModel
+        weahterViewController.disasterModel = disasterModel
         _ = weahterViewController.view
     }
 
@@ -27,51 +30,62 @@ class WeatherViewControllerTests: XCTestCase {
     }
 
     func test_天気予報がsunnyだったらImageViewのImageにsunnyが設定されること_TintColorがredに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .sunny, maxTemp: 0, minTemp: 0, date: Date())
+        weahterModel.fetchWeatherImpl = { (area, date, completion)  in
+            completion(.success(Response(weather: .sunny, maxTemp: 0, minTemp: 0, date: date)))
         }
-        
-        weahterViewController.loadWeather()
+        let testButton = UIButton()
+        weahterViewController.loadWeather(testButton)
         XCTAssertEqual(weahterViewController.weatherImageView.tintColor, R.color.red())
         XCTAssertEqual(weahterViewController.weatherImageView.image, R.image.sunny())
     }
     
     func test_天気予報がcloudyだったらImageViewのImageにcloudyが設定されること_TintColorがgrayに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .cloudy, maxTemp: 0, minTemp: 0, date: Date())
+        weahterModel.fetchWeatherImpl = { (area, date, completion)  in
+            completion(.success(Response(weather: .cloudy, maxTemp: 0, minTemp: 0, date: date)))
         }
-        
-        weahterViewController.loadWeather()
+        let testButton = UIButton()
+        weahterViewController.loadWeather(testButton)
         XCTAssertEqual(weahterViewController.weatherImageView.tintColor, R.color.gray())
         XCTAssertEqual(weahterViewController.weatherImageView.image, R.image.cloudy())
     }
     
     func test_天気予報がrainyだったらImageViewのImageにrainyが設定されること_TintColorがblueに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .rainy, maxTemp: 0, minTemp: 0, date: Date())
+        weahterModel.fetchWeatherImpl = { (area, date, completion) in
+            completion(.success(Response(weather: .rainy, maxTemp: 0, minTemp: 0, date: date)))
         }
-        
-        weahterViewController.loadWeather()
+        let testButton = UIButton()
+        weahterViewController.loadWeather(testButton)
         XCTAssertEqual(weahterViewController.weatherImageView.tintColor, R.color.blue())
         XCTAssertEqual(weahterViewController.weatherImageView.image, R.image.rainy())
     }
     
     func test_最高気温_最低気温がUILabelに設定されること() throws {
-        weahterModel.fetchWeatherImpl = { _ in
-            Response(weather: .rainy, maxTemp: 100, minTemp: -100, date: Date())
+        weahterModel.fetchWeatherImpl = { (area, date, completion) in
+            completion(.success(Response(weather: .sunny, maxTemp: 0, minTemp: 0, date: date)))
         }
-        
-        weahterViewController.loadWeather()
+        let testButton = UIButton()
+        weahterViewController.loadWeather(testButton)
         XCTAssertEqual(weahterViewController.minTempLabel.text, "-100")
         XCTAssertEqual(weahterViewController.maxTempLabel.text, "100")
     }
 }
 
+/*
+ func fetchWeather(at area: String, date: Date, completion: @escaping (Result<Response, WeatherError>) -> Void)
+ これを満たす必要あり
+*/
 class WeatherModelMock: WeatherModel {
+    var fetchWeatherImpl:  ((String, Date, @escaping (Result<Response, WeatherError>) -> Void) -> Void)?
     
-    var fetchWeatherImpl: ((Request) throws -> Response)!
-    
-    func fetchWeather(_ request: Request) throws -> Response {
-        return try fetchWeatherImpl(request)
+    func fetchWeather(at area: String, date: Date, completion: @escaping (Result<Response, WeatherError>) -> Void) {
+        fetchWeatherImpl?(area, date, completion)
+    }
+}
+
+class DisasterModelMock: DisasterModel {
+    var fetchDisasterImpl: ((@escaping (Result<String, DisasterError>) -> Void) -> Void)?
+
+    func fetchDisaster(completion: @escaping (Result<String, DisasterError>) -> Void) {
+        fetchDisasterImpl?(completion)
     }
 }
